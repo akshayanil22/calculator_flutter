de3974ad21826358647351a3890c3544091c216f
Akshay <akshay123akv@gmail.com>
2022-04-18 16:20:56 +0530

    Added functionalities

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
 
   final String text;
+  final Function onPress;
 
-  CustomButton({required this.text});
+  CustomButton({required this.text,required this.onPress});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,9 @@ class CustomButton extends StatelessWidget {
       width: 80,
       decoration: BoxDecoration(color: Colors.greenAccent,
           borderRadius: BorderRadius.circular(40)),
-      child: TextButton(child: Text(text,style: TextStyle(fontSize: 26,color: Colors.white),),onPressed:(){},),
+      child: TextButton(child: Text(text,style: TextStyle(fontSize: 26,color: Colors.white),),onPressed:(){
+        onPress(text);
+      },),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,80 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int firstNumber=0;
+
+  int secondNumber =0;
+
+  String history ='';
+
+  String display ='';
+
+  String result ='';
+
+  String operand ='';
+
+  void btnOnClick(String btnValue){
+    print(btnValue);
+    if(btnValue == 'Ac'){
+     setState(() {
+       display = '';
+       firstNumber = 0;
+       secondNumber = 0;
+       history ='';
+       result='';
+       operand='';
+     });
+    }else if(btnValue == '+' || btnValue == '-' || btnValue == 'x' || btnValue == '/'){
+     setState(() {
+       firstNumber = int.parse(display);
+       result='';
+       display = display+btnValue;
+       operand = btnValue;
+     });
+    }else if(btnValue == '='){
+      secondNumber = int.parse(display);
+      if(operand == '+'){
+        setState(() {
+          display = (firstNumber+secondNumber).toString();
+          history = firstNumber.toString()+operand+secondNumber.toString();
+        });
+      }if(operand == '-'){
+       setState(() {
+         display = (firstNumber-secondNumber).toString();
+         history = firstNumber.toString()+operand+secondNumber.toString();
+       });
+      }if(operand == 'x'){
+       setState(() {
+         display = (firstNumber*secondNumber).toString();
+         history = firstNumber.toString()+operand+secondNumber.toString();
+       });
+      }if(operand == '/'){
+        setState(() {
+          if(secondNumber!=0){
+            display = (firstNumber/secondNumber).round().toString();
+            history = firstNumber.toString()+operand+secondNumber.toString();
+          }else{
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Can\'t divisible by Zero')));
+          }
+        });
+      }
+      operand='=';
+    }else{
+      setState(() {
+        if(operand!='=') {
+          result = result + btnValue;
+          display = result;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +101,8 @@ class MyHomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-              Text('59+45',style: TextStyle(fontSize: 32,color: Colors.white60),),
-                Text('104',style: TextStyle(fontSize: 58,color: Colors.white),)
+              Text(history,style: TextStyle(fontSize: 32,color: Colors.white60),),
+                Text(display,style: TextStyle(fontSize: 58,color: Colors.white),)
             ],),
             decoration: BoxDecoration(
               color: Colors.blueGrey.shade600,
@@ -41,40 +113,40 @@ class MyHomePage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CustomButton(text: '7',),
-              CustomButton(text: '8',),
-              CustomButton(text: '9',),
-              CustomButton(text: 'x',),
+              CustomButton(text: '7',onPress: btnOnClick),
+              CustomButton(text: '8',onPress: btnOnClick),
+              CustomButton(text: '9',onPress: btnOnClick),
+              CustomButton(text: 'x',onPress: btnOnClick),
             ],
           ),
           SizedBox(height: 10,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CustomButton(text: '4',),
-              CustomButton(text: '5',),
-              CustomButton(text: '6',),
-              CustomButton(text: '-',),
+              CustomButton(text: '4',onPress: btnOnClick),
+              CustomButton(text: '5',onPress: btnOnClick),
+              CustomButton(text: '6',onPress: btnOnClick),
+              CustomButton(text: '-',onPress: btnOnClick),
             ],
           ),
           SizedBox(height: 10,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CustomButton(text: '1',),
-              CustomButton(text: '2',),
-              CustomButton(text: '3',),
-              CustomButton(text: '+',),
+              CustomButton(text: '1',onPress: btnOnClick),
+              CustomButton(text: '2',onPress:btnOnClick),
+              CustomButton(text: '3',onPress: btnOnClick),
+              CustomButton(text: '+',onPress: btnOnClick),
             ],
           ),
           SizedBox(height: 10,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CustomButton(text: 'Ac',),
-              CustomButton(text: '0',),
-              CustomButton(text: '=',),
-              CustomButton(text: '/',),
+              CustomButton(text: 'Ac',onPress: btnOnClick),
+              CustomButton(text: '0',onPress: btnOnClick),
+              CustomButton(text: '=',onPress: btnOnClick),
+              CustomButton(text: '/',onPress: btnOnClick),
             ],
           ),
           SizedBox(height: 10,)
